7d3bf518f544437a8a76014c176e1f6e7ed6229d
Partha <parthasdeyofficial@gmail.com>
2024-02-22 21:55:36 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 function App() {
   return (
     <div className="">
+      <span className="material-symbols-outlined absolute md:hidden top-3 left-3 text-white text-4xl">side_navigation</span>
       <div className='w-screen h-screen flex justify-between'>
         <aside className='hidden md:block w-[300px] h-screen bg-black border-r-2 border-t-2 border-b-2 border-white rounded-tr-2xl rounded-br-2xl p-2'></aside>
 
@@ -11,17 +12,35 @@ function App() {
             <h1 className='text-4xl font-semibold text-white font-["Poppins"]'>TaurusAI</h1>
           </div>
 
-          <div className='flex-col w-full h-[80%] px-4 md:px-20 py-4 md:py-10'>
-            <div className='bg-white w-full h-full rounded-2xl'></div>
+          <div className='flex-col w-full h-[80%] px-4 md:px-28 py-4 md:py-10'>
+            <div className='bg-white w-full h-full rounded-2xl overflow-y-auto flex-col space-y-2 px-4 py-2'>
+              <div className='bg-gray-800 w-full h-[100px] rounded-2xl'></div>
+              <div className='bg-black w-full h-[300px] rounded-2xl'></div>
+              <div className='bg-gray-800 w-full h-[100px] rounded-2xl'></div>
+              <div className='bg-black w-full h-[300px] rounded-2xl'></div>
+              <div className='bg-gray-800 w-full h-[100px] rounded-2xl'></div>
+              <div className='bg-black w-full h-[300px] rounded-2xl'></div>
+              <div className='bg-gray-800 w-full h-[100px] rounded-2xl'></div>
+              <div className='bg-black w-full h-[300px] rounded-2xl'></div>
+              <div className='bg-gray-800 w-full h-[100px] rounded-2xl'></div>
+              <div className='bg-black w-full h-[300px] rounded-2xl'></div>
+            </div>
           </div>
 
-          <div className='w-full h-[10%] flex justify-center items-center px-4 md:px-20'>
-            <div className='w-full h-[50px] border border-white rounded-2xl flex justify-between items-center'>
-              <div className='w-full h-full text-white px-4 py-2.5 text-xl'contentEditable="true" >Hello</div>
+          <div className='w-full h-[10%] flex justify-center items-center px-4 md:px-28'>
+            <div className='w-full max-h-48 border border-white rounded-2xl flex justify-between items-center'>
+              <div 
+                id='prompt_input_box' 
+                className='w-full h-full text-white px-4 py-2.5 text-xl focus:outline-none font-["Poppins"] overflow-y-auto'
+                placeholder='Enter your prompt..'
+                style={{ maxHeight: '100px' }}
+                contentEditable="true"
+              >
+              </div>
 
               <div>
-                <button className='px-2 py-1 text-white flex justify-center items-center bg-blue-600 rounded-2xl'>
-                <span class="material-symbols-outlined text-3xl">send</span>
+                <button className='px-2 py-1 text-white flex justify-center items-end bg-blue-600 hover:bg-blue-800 duration-300 hover:duration-300 rounded-2xl'>
+                  <span className="material-symbols-outlined text-3xl">send</span>
                 </button>
               </div>
             </div>
@@ -32,4 +51,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
